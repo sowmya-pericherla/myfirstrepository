aa7ed4f2ac4b6d1971fcf6d35919ce169e751d0e
sowmya-pericherla <36558683+sowmya-pericherla@users.noreply.github.com>
2018-02-17 11:57:27 +0000

    Update scriptforjenkins.sh

--- scriptforjenkins.sh
+++ scriptforjenkins.sh
@@ -1,7 +1,8 @@
 echo "starting project in jenkins through git"
-mkdir sowmyaproject
-vi abc
-vi 123
+mkdir /sowmyaproject
+cd /sowmyaproject
+touch abc
+touch 123
 echo #################
 echo ###########
 echo "end of the project"
